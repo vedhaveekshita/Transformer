0cefe26f9ae0b1e1b1e8ca645262868ae09bb9ff
vedhaveekshita <129981472+vedhaveekshita@users.noreply.github.com>
2024-01-08 12:07:28 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -4,11 +4,11 @@ st.header("Types of houses")
 col1,col2 = st.columns(2)
 with col1:
   st.subheader("type1")
-  st.image("./type 1.jpg",caption="type 1", width=300,use_column_width=True)
+  st.image("./type 1.png",caption="type 1", width=300,use_column_width=True)
   st.write("big house")
 with col2:
   st.subheader("type2")
-  st.image("./type 2.jpg",caption="type 2", width=300,use_column_width=True)
+  st.image("./type 2.png",caption="type 2", width=300,use_column_width=True)
   st.write("villa")
   
   
