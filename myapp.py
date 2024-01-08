bce1925a44860286b416cb62af3c393652680077
vedhaveekshita <129981472+vedhaveekshita@users.noreply.github.com>
2024-01-08 11:53:39 +0530

    myapp.py

---
+++ myapp.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.set_page_config(page_title="Houses")
+st.header("Types of houses")
+col1,col2 = st.columns(2)
+with col1:
+  st.subheader("type1")
+  st.image("./type 1.jpg",caption="type 1", width=300,use_column_width=True)
+  st.write("big house")
+with col2:
+  st.subheader("type2")
+  st.image("./type 2.jpg",caption="type 2", width=300,use_column_width=True)
+  st.write("villa")
+  
+  
